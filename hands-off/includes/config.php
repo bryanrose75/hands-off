9326e86f39f87935d249198c52dad1f3c8a799ac
bryanrose75 <46105717+bryanrose75@users.noreply.github.com>
2020-12-02 13:40:19 +0000

    Update config.php

--- hands-off/includes/config.php
+++ hands-off/includes/config.php
@@ -1,13 +1,13 @@
-//
-//<?php
-//	ob_start();
-//	session_start();
-//
-//	$timezone = date_default_timezone_set("Europe/London");
-//
-//	$con = mysqli_connect("localhost", "root", "", "chime");
-//
-//	if(mysqli_connect_errno()) {
-//		echo "Failed to connect: " . mysqli_connect_errno();
-//	}
-//?>
+
+<?php
+	ob_start();
+	session_start();
+
+	$timezone = date_default_timezone_set("Europe/London");
+
+	$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b1af505282471d", "ac3d484c", "heroku_0096838f3d2d118");
+
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . mysqli_connect_errno();
+	}
+?>
