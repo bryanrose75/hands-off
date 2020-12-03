403f41fc8b1f70906498315fec7cd5ce2bcc1b06
caoimheruddy <60384134+caoimheruddy@users.noreply.github.com>
2020-12-03 17:12:06 +0000

    Update index.php

--- hands-off/index.php
+++ hands-off/index.php
@@ -13,12 +13,45 @@ else {
 ?>
 
 <html>
-<head>
-    <title>Welcome to !our website</title>
-</head>
+	<head>
+		<title>	Hands-Off Homepage</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+		<body>
+		<script>
+		function myLogOut(){
+		location.replace("https://hands-off-app.herokuapp.com/register.php")
+		
+		}
+		</script>
+		
+			<div class="navBar">
+				<a href="index.html" class ="active" >Hands-Off</a>
 
-<body>
-    
-</body>
+				<button id="logOutButton" onclick="myLogOut()">
+				Log Out
+				
+				</button>
+			</div>
+			
+			<div class="welcomeContainer">
+			<h1> Welcome to Hands-Off!</h1>
+			<h2> The power is now in your hands.<br>
+			You will now have the capability to control a video purely by hand gestures.</h2>
+			</div>
+			
+			<div class="tutorialContainer">
+			<h1>There will be a tutorial video here to demonstrate how to use our website!</h1>
+			</div>
+			
+			<div class="videoSelection">
+			<h1>Please select a video to get started!</h1>
+			<div id="firstVid">video one</div>
+			<div id="secondVid">video two</div>
+			<div id="thirdVid">video three</div>
+			</div>
+		</body>
 
 </html>
