9a15cb48ecc279cae399e646a1658e66960d1927
leebrady <55153931+leebrady@users.noreply.github.com>
2020-12-04 16:22:43 +0000

    New index test

--- hands-off/includes/config.php
+++ hands-off/includes/config.php
@@ -1,10 +1,11 @@
+
 <?php
 	ob_start();
 	session_start();
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "root", "", "chime");
+	$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b1af505282471d", "ac3d484c", "heroku_0096838f3d2d118");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
--- hands-off/register.php
+++ hands-off/register.php
@@ -17,7 +17,7 @@
 
 <html>
 <head>
-	<title>Welcome to Touch!</title>
+	<title>Welcome to Hands-Off!</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
