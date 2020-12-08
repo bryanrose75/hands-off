3bee4872133f588900fba8eb7986fe71a9a179b0
caoimheruddy <60384134+caoimheruddy@users.noreply.github.com>
2020-12-08 09:08:53 +0000

    Update index.php

--- hands-off/index.php
+++ hands-off/index.php
@@ -127,61 +127,7 @@ else {
 	        </div>
         </div>
 
-        <!-- Section 1 -->
-        <div class="section-1-container section-container">
-	        <div class="container">
-	            <div class="row">
-	                <div class="col section-1 section-description wow fadeIn">
-	                    <h2>Section 1</h2>
-	                    <div class="divider-1 wow fadeInUp"><span></span></div>
-	                </div>
-	            </div>
-	            <div class="row">
-                	<div class="col-md-4 section-1-box wow fadeInUp">
-                		<div class="row">
-                			<div class="col-md-4">
-			                	<div class="section-1-box-icon">
-			                		<i class="fas fa-magic"></i>
-			                	</div>
-		                	</div>
-	                		<div class="col-md-8">
-	                    		<h3>Branding</h3>
-	                    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
-	                    	</div>
-	                    </div>
-                    </div>
-                    <div class="col-md-4 section-1-box wow fadeInDown">
-	                	<div class="row">
-                			<div class="col-md-4">
-			                	<div class="section-1-box-icon">
-			                		<i class="fas fa-cog"></i>
-			                	</div>
-		                	</div>
-	                		<div class="col-md-8">
-	                    		<h3>Web design</h3>
-	                    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
-	                    	</div>
-	                    </div>
-                    </div>
-                    <div class="col-md-4 section-1-box wow fadeInUp">
-	                	<div class="row">
-                			<div class="col-md-4">
-			                	<div class="section-1-box-icon">
-			                		<i class="fab fa-twitter"></i>
-			                	</div>
-		                	</div>
-	                		<div class="col-md-8">
-	                    		<h3>Social media</h3>
-	                    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
-	                    	</div>
-	                    </div>
-                    </div>
-	            </div>
-	        </div>
-        </div>
-
-
-
+       
 		<!-- Section 4 -->
         <div class="section-4-container section-container section-container-image-bg">
 	        <div class="container">
