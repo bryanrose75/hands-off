7252fef804cbce1bb8a072b70d1e05aa52255809
Leezy <leebrady0114@gmail.com>
2020-12-14 17:15:08 +0000

    Update scripts.js

--- hands-off/assets/js/scripts.js
+++ hands-off/assets/js/scripts.js
@@ -29,7 +29,7 @@ jQuery(document).ready(function() {
     /*
         Background slideshow
     */
-	$('.top-content').backstretch("assets/images/backgrounds/1.jpg");
+	$('.top-content').backstretch("/hands-off/assets/images/backgrounds/1.jpg");
     $('.section-4-container').backstretch("assets/images/backgrounds/2.jpg");
     
     /*
