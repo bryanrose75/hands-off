5f2aabf1f6169a24c2f60d8e155525ea9492c071
Leezy <leebrady0114@gmail.com>
2020-12-15 13:01:30 +0000

    Update index.php

--- hands-off/index.php
+++ hands-off/index.php
@@ -96,7 +96,8 @@ else {
 	       					<div class="carousel-inner">
 	       						<div class="carousel-item active">
 	       							<div class="embed-responsive embed-responsive-16by9">
-	       								<iframe class="embed-responsive-item" src="assets/vid/Hands Off.mp4" allowfullscreen></iframe>
+									   <video controls>
+	       								<class="embed-responsive-item" src="assets/vid/Hands Off.mp4" type="video/mp4">
 	       							</div>
 	       						</div>
 	       						<div class="carousel-item">
