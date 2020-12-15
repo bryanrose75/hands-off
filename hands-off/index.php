3792f872beaf4f538c0d1a430d683a3650b604bf
Leezy <leebrady0114@gmail.com>
2020-12-15 12:58:51 +0000

    Update index.php

--- hands-off/index.php
+++ hands-off/index.php
@@ -84,93 +84,49 @@ else {
 				<!-- End title and description row -->
 	
 	            <!-- Carousel row -->
-	            <!--Carousel Wrapper-->
-<div id="video-carousel-example2" class="carousel slide carousel-fade" data-ride="carousel">
-  <!--Indicators-->
-  <ol class="carousel-indicators">
-    <li data-target="#video-carousel-example2" data-slide-to="0" class="active"></li>
-    <li data-target="#video-carousel-example2" data-slide-to="1"></li>
-    <li data-target="#video-carousel-example2" data-slide-to="2"></li>
-  </ol>
-  <!--/.Indicators-->
-  <!--Slides-->
-  <div class="carousel-inner" role="listbox">
-    <!-- First slide -->
-    <div class="carousel-item active">
-      <!--Mask color-->
-      <div class="view">
-        <!--Video source-->
-        <video class="video-fluid" muted>
-          <source src="hands-off\assets\vid\Hands Off.mp4" type="video/mp4" />
-        </video>
-        <div class="mask rgba-indigo-light"></div>
-      </div>
-
-      <!--Caption-->
-      <div class="carousel-caption">
-        <div class="animated fadeInDown">
-          <h3 class="h3-responsive">Light mask</h3>
+	            <div class="row">
+	                <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+	                	<!-- Carousel -->
+	                	<div id="carousel-example" class="carousel slide">
+	       					<ol class="carousel-indicators">
+	       						<li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+	       						<li data-target="#carousel-example" data-slide-to="1" ></li>
+	       						<li data-target="#carousel-example" data-slide-to="2" ></li>
+	       					</ol>
+	       					<div class="carousel-inner">
+	       						<div class="carousel-item active">
+	       							<div class="embed-responsive embed-responsive-16by9">
+	       								<iframe class="embed-responsive-item" src="assets/vid/Hands Off.mp4" allowfullscreen></iframe>
+	       							</div>
+	       						</div>
+	       						<div class="carousel-item">
+	       							<div class="embed-responsive embed-responsive-16by9" >
+	       								<iframe class="embed-responsive-item"src="assets/vid/vid1.mp4" allowfullscreen></iframe>
+	       							</div>
+	       						</div>
+	       						<div class="carousel-item" >
+	       							<div class="embed-responsive embed-responsive-16by9">
+	       								<iframe class="embed-responsive-item" src="assets/vid/vid2.mp4" allowfullscreen></iframe>
+	       							</div>
+	       						</div>
+								
+	       					</div>
+							<a class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+	       				</div>
+	                	<!-- End carousel -->
+	                </div>
+	            </div>
+	            <!-- End carousel row -->
+	        </div>
         </div>
-      </div>
-      <!--Caption-->
-    </div>
-    <!-- /.First slide -->
 
-    <!-- Second slide -->
-    <div class="carousel-item">
-      <!--Mask color-->
-      <div class="view">
-        <!--Video source-->
-        <video class="video-fluid" muted>
-          <source src="https://mdbootstrap.com/img/video/animation-intro.mp4" type="video/mp4" />
-        </video>
-        <div class="mask rgba-purple-slight"></div>
-      </div>
-
-      <!--Caption-->
-      <div class="carousel-caption">
-        <div class="animated fadeInDown">
-          <h3 class="h3-responsive">Super light mask</h3>
-        </div>
-      </div>
-      <!--Caption-->
-    </div>
-    <!-- /.Second slide -->
-
-    <!-- Third slide -->
-    <div class="carousel-item">
-      <!--Mask color-->
-      <div class="view">
-        <!--Video source-->
-        <video class="video-fluid" muted>
-          <source src="https://mdbootstrap.com/img/video/cube.mp4" type="video/mp4" />
-        </video>
-        <div class="mask rgba-black-strong"></div>
-      </div>
-
-      <!--Caption-->
-      <div class="carousel-caption">
-        <div class="animated fadeInDown">
-          <h3 class="h3-responsive">Strong mask</h3>
-        </div>
-      </div>
-      <!--Caption-->
-    </div>
-    <!-- /.Third slide -->
-  </div>
-  <!--/.Slides-->
-  <!--Controls-->
-  <a class="carousel-control-prev" href="#video-carousel-example2" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#video-carousel-example2" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-  <!--/.Controls-->
-</div>
-<!--Carousel Wrapper-->
        
 
         <!-- Javascript -->
